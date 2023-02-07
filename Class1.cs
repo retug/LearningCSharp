0d528ae80d7bef076a7bbea609797c08a286f11d
Austin Guter <45467091+retug@users.noreply.github.com>
2023-02-06 21:09:11 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Data.Text;
+
+
+namespace WindowsFormsApp1
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        //This is the constructor, redefine the point?
+
+        public Point(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    public class PointVector
+    {
+        public List<double> XYZ {get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+
+        //This is the constructor, redefine the point?
+
+        public PointVector(List<double> xyz)
+        {
+            X = xyz[0];
+            Y = xyz[1];
+            Z = xyz[2];
+        }
+    }
+    public class GlobalCoordinateSystem
+    {
+        public List<double> XYZ { get; set; }
+        public List<double> Vector { get; set; }
+        public double hyp { get; set; }
+        public double[,] R { get; set; }
+        public string inverseMatrixText { get; set; }
+        public Matrix<double> customMatrix { get; set; }
+        //This is the constructor, redefine the point?
+        public GlobalCoordinateSystem(List<double> xyz, List<double> vector)
+        {
+            hyp = Math.Sqrt((vector[0]* vector[0] + vector[1]* vector[1]));
+
+            R = new double[,] { { vector[0] / hyp, -vector[1] / hyp, 0 }, { vector[1] / hyp, vector[0] / hyp, 0 }, { 0, 0, 1 } };
+
+            //Matrix<double> customMatrix = Matrix<double>.Build.DenseOfArray(R);
+            //Matrix<double> inverseMyCustomMatrix = customMatrix.Inverse();
+
+            //string inverseMatrixText = customMatrix.ToString("F2");
+
+
+        }
+    }
+
+}
+    
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string name = "test";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = "John Doe";
+           
+            Point myPoint = new Point(1.2,0.6,0.25);
+
+
+            //this syntax is a bit confusing
+            List<double> valueXYZ = new List<double>() { 1.0, 0.0, 0.0 };
+            
+            PointVector pointVector = new PointVector(valueXYZ);
+
+            List<double> vector = new List<double>() { 1.0, 1.0, 0 };
+
+            GlobalCoordinateSystem globalCoordinateSystem = new GlobalCoordinateSystem(valueXYZ, vector);
+            int x = 2;
+            //MessageBox.Show(globalCoordinateSystem.R.ToString());
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(name);
+        }
+    }
+}
