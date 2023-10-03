0f6ebbc68737283f6682a0c3d1d46d0f5ec4048d
Austin Guter <45467091+retug@users.noreply.github.com>
2023-10-02 20:49:57 -0600

    Add files via upload

--- WPF_XAML/mainViewModel.cs
+++ WPF_XAML/mainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
+using LiveChartsCore.Drawing;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
@@ -27,4 +28,24 @@ public class ViewModel : ObservableObject
             Padding = new LiveChartsCore.Drawing.Padding(15),
             Paint = new SolidColorPaint(SKColors.DarkSlateGray)
         };
+
+    public ISeries[] Series2 { get; set; } =
+    {
+        new LineSeries<double>
+        {
+            Values = new double[] { 1, 10, -3, 5, 3, 4, 6 },
+            Fill = null,
+            Stroke = new SolidColorPaint(SKColors.Red)
+        }
+    };
+
+    public LabelVisual Title2 { get; set; } =
+        new LabelVisual
+        {
+            Text = "My chart title",
+            TextSize = 25,
+            Padding = new LiveChartsCore.Drawing.Padding(15),
+            
+        };
 }
+
