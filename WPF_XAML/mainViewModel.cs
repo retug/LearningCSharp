389e6d6a8788abd335299b17f62b016813416fa2
Austin Guter <45467091+retug@users.noreply.github.com>
2023-10-03 08:02:16 -0600

    Add files via upload

--- WPF_XAML/mainViewModel.cs
+++ WPF_XAML/mainViewModel.cs
@@ -1,16 +1,47 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
+using LiveChartsCore.Defaults;
 using LiveChartsCore.Drawing;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
 using SkiaSharp;
-using System.Windows.Media;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WpfApp1;
 
+
+public class Data
+{
+    public void ProcessData()
+    {
+        List<double> testX = new List<double> { 0, 20, 0, 20 };
+        List<double> testY = new List<double> { 0, 0, 20, 20 };
+
+        for (int i = 0; i < testX.Count(); i++)
+        {
+
+        }
+    }
+    
+              
+}
+
+
 public class ViewModel : ObservableObject
 {
+    List<double> testX = new List<double> { 0, 20, 0, 20 };
+    List<double> testY = new List<double> { 0, 0, 20, 20 };
+
+    LiveChartsCore.Defaults.ObservablePoint test = new LiveChartsCore.Defaults.ObservablePoint();
+    
+    
+
+
+
     public ISeries[] Series { get; set; } =
     {
         new LineSeries<double>
@@ -47,5 +78,18 @@ public class ViewModel : ObservableObject
             Padding = new LiveChartsCore.Drawing.Padding(15),
             
         };
+    // https://livecharts.dev/docs/WPF/2.0.0-rc1/samples.general.sections2
+
+
+    public ISeries[] Series3 { get; set; } =
+    {
+        new LineSeries<double>
+        {
+            
+            //ChartValues<LiveChartsCore.Defaults.ObservablePoint> areaPoints = new LiveCharts.ChartValues<LiveCharts.Defaults.ObservablePoint>();
+        }
+    };
+
+
 }
 
