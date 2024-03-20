28b1118d4da2cf7c1cca1e3fb2267d3f34e9f7d1
Austin Guter <45467091+retug@users.noreply.github.com>
2024-03-19 21:56:37 -0600

    Add files via upload

--- MVVM/RevitFramingModel.cs
+++ MVVM/RevitFramingModel.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace etabsRevitCnx
 {
-    public class RevitFramingModel
+    public class RevitFramingModel : INotifyPropertyChanged
     {
-        public string Name { get; set; }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
